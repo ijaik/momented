ebc383bee5a85519b4086d39bc2d61c8784e0bd4
ijaik <fghijaik@gmail.com>
2026-08-30 11:03:58 +0530

    Updated

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,8 +6,8 @@ export default function MainLayout({ children }: { children: ReactNode }) {
       <Navbar />
       <div className="grow pt-24">{children}</div>
       <footer className="shrink-0 py-10 text-center text-sm text-zinc-500 border-t border-zinc-200 dark:border-zinc-900 mt-20">
-        <span className="font-leckerli tracking-tight">Momented</span> with 🩶
-        by Jai.
+        <span className="font-leckerli tracking-tight">Momented</span> with ♥︎ by
+        Jai.
       </footer>
     </div>
   );
