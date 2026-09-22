92ea1bf398b2e499dcc0100abae2cbcb83aa4573
ijaik <fghijaik@gmail.com>
2026-09-22 21:32:45 +0530

    Updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactHooks from "eslint-plugin-react-hooks";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  reactHooks.configs.flat.recommended,
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 export default eslintConfig;
